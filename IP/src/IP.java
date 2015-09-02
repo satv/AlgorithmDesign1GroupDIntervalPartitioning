baa10a8ad531dcf249d2b55a8f34d5f1f8554516
AaronGitH <agor@itu.dk>
2015-09-02 16:36:07 +0200

    the sorting has to do quick ;-)

--- IP/src/IP.java
+++ IP/src/IP.java
@@ -1,9 +1,10 @@
 
+import java.awt.geom.QuadCurve2D;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 /*
@@ -14,6 +15,7 @@ import java.util.Scanner;
 /**
  *
  * @author Rene Anda Nielsen <rann@itu.dk>
+ * @author Aaron Gornott <agor@itu.dk>
  */
 public class IP {
 
@@ -23,14 +25,14 @@ public class IP {
     private static Interval[] intervals;
     private static Interval interval;
     
-    private static Queue<Integer> sortedIntervalIDs = new LinkedList();
+    private static ArrayList<Integer> sortedIntervalIDs = new ArrayList();
         
     private static int d; // no of partitions
     private static ArrayList<ArrayList<Integer>> partitionIntervals = new ArrayList();
 
     public static void main(String[] args) throws FileNotFoundException {
         IP ip = new IP();
-        File file = new File("ip-1.in");
+        File file = new File("ip-rand-1k.in");
         sc = new Scanner(file);
         //sc = new Scanner(System.in);
         int id = 0;
@@ -52,17 +54,30 @@ public class IP {
                 String[] times = line.split(" ");
                 int start = Integer.parseInt(times[0]);
                 int end = Integer.parseInt(times[1]);
-                interval = new Interval(start,end );
+                interval = new Interval(id, start,end );
                 intervals[id] = interval;
                 id++;
             }
         }
-        for (Interval interval : intervals) {
-            System.out.println(interval.start + " " + interval.end);
-        }
+//        for (Interval interval : intervals) {
+//            System.out.println(interval.start + " " + interval.end);
+//        }
         
         
         // SORT lectures by start time so that s1 ≤ s2 ≤ ... ≤ sn.
+        
+        // #######################################
+        //Arrays.sort(intervals);
+        
+        
+        
+        
+//        System.out.println("Sorting order:\n");
+//        for(Interval i: intervals){
+//            System.out.println(i.start);
+//        }
+        
+//####################################        
         for(int i = 0; i < n; i++){ // this is O(n^n) !!!!!!! DANGER DANGER
             int smallestID = -1;
             int smallestVal = Integer.MAX_VALUE;
@@ -74,24 +89,27 @@ public class IP {
             }
             sortedIntervalIDs.add(smallestID);
         }
+ //################################################################       
+        
         // d <- 0
         d = 0;
         // FOR j = 1 TO n
-        for(int j = 1; j <= 0; j++){
+        for(int j = 0; j < n; j++){
             // IF lecture j is compatible with some classroom
-            int compatibleID = isCompatible(intervals[j], partitionIntervals);
+            int sortedJ = sortedIntervalIDs.get(j);
+            int compatibleID = isCompatible(intervals[sortedJ], partitionIntervals);
             if(compatibleID != -1){
                 // Schedule lecture j in any such classroom k. 
-                partitionIntervals.get(compatibleID).add(j);
-                intervals[j].partition = compatibleID;
+                partitionIntervals.get(compatibleID).add(sortedJ);
+                intervals[sortedJ].partition = compatibleID;
             }
             // ELSE
             else{
                 // Allocate a new classroom d + 1.                
                 partitionIntervals.add(new ArrayList());
                 // Schedule lecture j in classroom d + 1.
-                partitionIntervals.get(d).add(j);
-                intervals[j].partition = d;
+                partitionIntervals.get(d).add(sortedJ);
+                intervals[sortedJ].partition = d;
                 // d <- d + 1
                 d++;
             }
@@ -99,6 +117,24 @@ public class IP {
         // RETURN schedule.
         
         
+        System.out.println("\nOutput:\n" + n + "\n");
+        for(Interval i: intervals){
+            System.out.println(i.start + " " + i.end + " " + i.partition);
+        }
+        
+        // === the real algo from the book ===
+        // Sort the intervals by their start times, breaking ties arbitrarily
+        // Let I1, I2, . . . , In denote the intervals in this order
+        // For j = 1, 2, 3, . . . , n
+        // For each interval Ii that precedes Ij in sorted order and overlaps it
+        // Exclude the label of Ii from consideration for Ij
+        // Endfor
+        // If there is any label from {1, 2, . . . , d} that has not been excluded then
+        // Assign a nonexcluded label to Ij
+        // Else
+        // Leave Ij unlabeled
+        // Endif
+        // Endfor
     }
     
     /**
@@ -110,10 +146,10 @@ public class IP {
     private static int isCompatible(Interval interval, ArrayList<ArrayList<Integer>> partitionIntervals){
         int fittingPartition = -1;        
         // search free space -- Big O(N); because max iteration over all intervals
-        for(int i = 0; i < d; i++){
+        for(int i = 1; i <= d; i++){ // not start with d=0
             boolean  noOverlaping = true;
-            for(int j = 0; j < partitionIntervals.get(i).size(); j++){
-                int other = partitionIntervals.get(i).get(j);
+            for(int j = 0; j < partitionIntervals.get(i-1).size(); j++){
+                int other = partitionIntervals.get(i-1).get(j);
                 if(
                     (intervals[other].start <= interval.start
                         &&
@@ -124,25 +160,34 @@ public class IP {
                         intervals[other].start < interval.end)
                     ){ 
                     noOverlaping = false;
-                } 
+                }
             }
             if(noOverlaping){
-               fittingPartition = i; 
+               fittingPartition = i-1; 
                break;
             }            
         }
         return fittingPartition;
     }
 
-    private static class Interval {
+    private static class Interval implements Comparable{
 
+        int id;
         int start;
         int end;
         int partition;
 
-        public Interval(int start, int end) {
+        public Interval(int id, int start, int end) {
+            this.id = id;
             this.start = start;
             this.end = end;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if(this.start < ((Interval)o).start){ return -1;}
+            if(this.start > ((Interval)o).start){ return 1;} 
+            return 0;
         }
     }
 }
