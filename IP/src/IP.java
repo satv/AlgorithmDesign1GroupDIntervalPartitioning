6bbc16451602aed749d53ce3abd3ebd5d0b83528
AaronGitH <agor@itu.dk>
2015-09-07 14:59:31 +0200

    N log N ....

--- IP/src/IP.java
+++ IP/src/IP.java
@@ -1,12 +1,12 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
- *
  * @author Rene Anda Nielsen <rann@itu.dk>
  * @author Aaron Gornott <agor@itu.dk>
  * @author Sarah
@@ -14,29 +14,32 @@ import java.util.Scanner;
 public class IP {
 
     private static Scanner sc;
-    private static int n; // number of intervals
-    private static int k; // number of partitions
-    private static Interval[] intervals;
-    private static Interval interval;
     
-    //private final static ArrayList<Integer> sortedIntervalIDs = new ArrayList();
+    private static int n; // number of intervals
+    private static Interval[] intervals;
         
-    private static int d; // no of partitions
-    private final static ArrayList<ArrayList<Integer>> partitionIntervals = new ArrayList();
+    private static int d; // number of partitions
+    private static final Comparator<Integer> shortestIntervalEndComparator = new Comparator<Integer>(){
+        public int compare(Integer o1, Integer o2) {
+            Integer i1 = intervals[partitionIntervals.get(o1).get(partitionIntervals.get(o1).size()-1)].end;
+            Integer i2 = intervals[partitionIntervals.get(o2).get(partitionIntervals.get(o2).size()-1)].end;
+            return i1.compareTo(i2);
+        }
+    };
+    private static PriorityQueue<Integer> shortestIntervalEnd = new PriorityQueue(shortestIntervalEndComparator);
+    private static final ArrayList<ArrayList<Integer>> partitionIntervals = new ArrayList();
 
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("ip-rand-1k.in");
+        File file = new File("ip-rand-1M.in");
         sc = new Scanner(file);
         int id = 0;
         int lineNr = 0;
         // read file
         while (sc.hasNextLine()) {
-
             String line = sc.nextLine().trim();
             if (line.length() == 0) {
                 continue;
             }
-
             if (lineNr == 0) {
                 n = Integer.parseInt(line);
                 System.out.println("N is: " + n);
@@ -46,20 +49,24 @@ public class IP {
                 String[] times = line.split(" ");
                 int start = Integer.parseInt(times[0]);
                 int end = Integer.parseInt(times[1]);
-                interval = new Interval(id, start,end, "sort by start time");
+                Interval interval = new Interval(id, start,end, "sort by start time");
                 intervals[id] = interval;
                 id++;
             }
         }
         
         // SORT lectures by start time so that s1 ≤ s2 ≤ ... ≤ sn.
-        Arrays.sort(intervals);    
+        Arrays.sort(intervals);   
         // d <- 0
-        d = 0;
+        // Add first element
+        partitionIntervals.add(new ArrayList());
+        partitionIntervals.get(0).add(0);
+        intervals[0].partition = 0;
+        shortestIntervalEnd.add(0);
+        d = 1;
         // FOR j = 1 TO n
-        for(int j = 0; j < n; j++){
+        for(int j = 1; j < n; j++){
             // IF lecture j is compatible with some classroom
-            //// int sortedJ = sortedIntervalIDs.get(j);
             int compatibleID = isCompatible(intervals[j], partitionIntervals);
             if(compatibleID != -1){
                 // Schedule lecture j in any such classroom k. 
@@ -74,66 +81,36 @@ public class IP {
                 partitionIntervals.get(d).add(j);
                 intervals[j].partition = d;
                 // d <- d + 1
-                d++;
+                d++;  
             }
+            //// taking care of k
+            if(shortestIntervalEnd.peek() == compatibleID)
+                shortestIntervalEnd.add(shortestIntervalEnd.poll()); // Damn Magic! 
+            else
+                shortestIntervalEnd.add( d - 1 );
         }
         // RETURN schedule.
         for(Interval i: intervals)
             i.comparator = "sort by id";
         Arrays.sort(intervals);
         
-        System.out.println("\nOutput:\n" + n + "\n");
+        System.out.println("\nOutput:\n" + d + "\n");
         for(Interval i: intervals){
             System.out.println(i.start + " " + i.end + " " + i.partition);
         }
-        
-        // === the real algo from the book ===
-        // Sort the intervals by their start times, breaking ties arbitrarily
-        // Let I1, I2, . . . , In denote the intervals in this order
-        // For j = 1, 2, 3, . . . , n
-        // For each interval Ii that precedes Ij in sorted order and overlaps it
-        // Exclude the label of Ii from consideration for Ij
-        // Endfor
-        // If there is any label from {1, 2, . . . , d} that has not been excluded then
-        // Assign a nonexcluded label to Ij
-        // Else
-        // Leave Ij unlabeled
-        // Endif
-        // Endfor
     }
     
     /**
- * 
- * @param interval
- * @param partitionIntervals
- * @return -1 = not fitting; any other value = index of partition
- */
+    * @param interval
+    * @param partitionIntervals
+    * @return -1 = not fitting; any other value = index of partition
+    */
     private static int isCompatible(Interval interval, ArrayList<ArrayList<Integer>> partitionIntervals){
-        int fittingPartition = -1;        
-        // search free space -- Big O(N); because max iteration over all intervals
-        for(int i = 1; i <= d; i++){ // not start with d=0
-            boolean  noOverlaping = true;
-            for(int j = 0; j < partitionIntervals.get(i-1).size(); j++){
-                int other = partitionIntervals.get(i-1).get(j);
-                if(
-                    (intervals[other].start <= interval.start
-                        &&
-                        intervals[other].end > interval.start)
-                    ||                        
-                    (intervals[other].start > interval.start
-                        && 
-                        intervals[other].start < interval.end)
-                    ){ 
-                    noOverlaping = false;
-                    break;
-                }
-            }
-            if(noOverlaping){
-               fittingPartition = i-1; 
-               break;
-            }            
-        }
-        return fittingPartition;
+        int k = shortestIntervalEnd.peek();        
+        if(intervals[partitionIntervals.get(k).get(partitionIntervals.get(k).size()-1)].end <= interval.start)
+            return k;
+        else
+            return -1;
     }
 
     private static class Interval implements Comparable{
@@ -165,5 +142,5 @@ public class IP {
             }
             throw new IllegalArgumentException("Your comparator is not valid!");
         }
-    }
+    }    
 }
